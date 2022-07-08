3b459081da07a1d581950f82cfe8bcfed4d05816
Lazzadi <>
2022-07-08 21:04:08 +0300

    second commit

--- main.py
+++ main.py
@@ -3,17 +3,46 @@ import shape_calculator
 from unittest import main
 
 
-rect = shape_calculator.Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
+# rect = shape_calculator.Rectangle(10, 5)
+# print(rect.get_area())
+# rect.set_height(3)
+# print(rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
 
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
+# sq = shape_calculator.Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
+
+
+
+# rect = shape_calculator.Rectangle(16, 8)
+# sq = shape_calculator.Square(4)
+# print (sq.get_area())
+# print (rect.get_amount_inside(sq))
+# print (f'Area:{rect.get_area()}')  
+# print (f'Perimeter:{rect.get_perimeter()}') 
+# print (f'Diagonal:{rect.get_diagonal()}') 
+
+# rect.set_width(3)
+# rect.set_height('agsfrdg')
+# print(rect.get_perimeter())
+
+# print(rect.get_picture())
+# print(rect)
+
+# sq = shape_calculator.Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
 
 
 # Run unit tests automatically
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,75 @@
 class Rectangle:
 
+  
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+
+  
+  def set_width(self, width):
+    self.width = width
+
+  
+  def set_height(self, height):
+    self.height = height
+
+  
+  def get_area(self):
+    return (self.width*self.height)
+
+  
+  def get_perimeter(self):
+    return (2*self.width + 2*self.height)
+
+  
+  def get_diagonal(self):
+    return ((self.width**2 + self.height**2)**.5)
+
+    
+  def get_picture(self):
+    picture = ''
+    if(self.width<=50 and self.height<=50):
+      for i in range (0, self.height):
+        for j in range (0, self.width):
+          picture += '*'
+        picture += '\n'
+      # picture = picture.rstrip('\n')
+      return (picture)
+    else:
+      return('Too big for picture.')
+
+  
+  def get_amount_inside(self, shape):
+    if (shape.height>self.height or shape.width>self.width):
+      return 0
+    else:
+      return (int(self.height/shape.height) *  int(self.width/shape.width))
+
+  
+  def __repr__(self):
+    return (f'Rectangle(width={self.width}, height={self.height})')
+
+class Square(Rectangle):
+  
+  def __init__(self, side):
+    self.width = side
+    self.height = side
+
+  def set_side(self, newSide):
+    self.width = newSide
+    self.height = newSide
+
+  def set_width(self, width):
+    self.width = width
+    self.height = width
+
+  def set_height(self, height):
+    self.height = height
+    self.width = height    
+
+  def __repr__(self):
+    return (f'Square(side={self.width})')
 
 
 
-class Square:
+  #TODO: Additionally, the set_width and set_height methods on the Square class should set both the width and height.
